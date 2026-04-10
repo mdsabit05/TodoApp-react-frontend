f324fc00105a5579a30c275ea5b97d114def5238
Md Sabit <72883223+mdsabit05@users.noreply.github.com>
2026-04-10 20:10:48 +0530

    Add files via upload

---
+++ frontend/components/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Navbar = ({ HomeUI }) => {
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.removeItem("token"); // 🔥 token delete
+    navigate("/login"); // 🔥 redirect
+  };
+  const isLoggedIn = localStorage.getItem("token");
+  return (
+    <nav className="navbar">
+      <div className="logo">TaskFlow</div>
+
+      <ul className="menu">
+        <li onClick={() => navigate("/")}>Home</li>
+        <li>My Tasks</li>
+        <li>Analytics</li>
+      </ul>
+
+      <div className="nav-actions">
+        {!isLoggedIn ? (
+          <button onClick={() => navigate("/login")}>Login</button>
+        ) : (
+          <button onClick={logout}>Logout</button>
+        )}
+        <button onClick={() => navigate("/register")}>Sign Up</button>
+        <button className="settings">⚙ Settings</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ frontend/components/components/TodoInput.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const TodoInput = ({
+  todo,
+  setTodo,
+  addTodo,
+  category,
+  setCategory,
+  error,
+  addLoading
+}) => {
+  return (
+    <div className="todo-input">
+      <input
+        type="text"
+        placeholder="Add a new task..."
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && todo.trim() !== "") {
+            addTodo();
+          }
+        }}
+        className={error.todo ? "input error" : "input"}
+      />
+
+      <input
+        type="text"
+        placeholder="Enter category"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+        className={error.category ? "input error" : "input"}
+      />
+
+      <button onClick={addTodo} disabled={addLoading}> 
+  {addLoading ? "Adding..." : "Add"}
+</button>
+    </div>
+  );
+};
+
+export default TodoInput;
