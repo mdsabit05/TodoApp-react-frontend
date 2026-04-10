483a5fad6535fab48ad79a56f2f312728a515c68
Md Sabit <72883223+mdsabit05@users.noreply.github.com>
2026-04-10 22:46:17 +0530

    Add files via upload

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import { API } from "../API";
+import { useNavigate } from "react-router-dom";
+
+
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const login = async () => {
+    try {
+      const res = await API.post("/api/auth/login", {
+        email,
+        password,
+      });
+
+      const token = res.data.token;
+      localStorage.setItem("token", token);
+
+      alert("Login successful ✅");
+
+      navigate("/");
+       
+    } catch (err) {
+      alert("Login failed ❌");
+    }
+  };
+
+  return (
+    <div style={styles.container}>
+      <div style={styles.card}>
+        <h2 style={styles.title}>Login</h2>
+
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          style={styles.input}
+        />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          style={styles.input}
+        />
+
+        <button onClick={login} style={styles.button}>
+          Login
+        </button>
+         <p>
+          Don't have account?{" "}
+          <span style={styles.span} onClick={() => navigate("/Register")}>Register</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
+
+const styles = {
+  container: {
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+   background: "#0f172a"
+  },
+  card: {
+    background: "#1e293b",
+    padding: "30px",
+    borderRadius: "12px",
+    boxShadow: "0 5px 15px rgba(0,0,0,0.1)",
+    width: "300px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "15px",
+  },
+  title: {
+    textAlign: "center",
+
+  },
+  input: {
+    padding: "10px",
+    borderRadius: "8px",
+    border: "1px solid #ccc",
+  },
+  button: {
+    padding: "10px",
+    borderRadius: "8px",
+    border: "none",
+    background: "#4CAF50",
+    color: "#fff",
+    cursor: "pointer",
+  },
+  span: {
+    color: "#3b82f6",
+  cursor: "pointer"
+  }
+};
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { API } from "../API";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleRegister = async () => {
+    try {
+      setLoading(true);
+      setError("");
+
+      await API.post("/api/auth/register", {
+        email,
+        password,
+      });
+
+      alert("Registered Successfully ✅");
+      navigate("/login");
+
+    } catch (err) {
+      setError(err.response?.data || "Registration failed");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="auth-container">
+      <div className="auth-card">
+        <h2>Create Account</h2>
+
+        {error && <p className="error">{error}</p>}
+
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button onClick={handleRegister} disabled={loading}>
+          {loading ? "Creating..." : "Register"}
+        </button>
+
+        <p>
+          Already have account?{" "}
+          <span onClick={() => navigate("/login")}>Login</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
