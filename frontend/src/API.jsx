0f42fa4a8ec969e30dbe4a9a5c3837ded91709c7
Md Sabit <72883223+mdsabit05@users.noreply.github.com>
2026-04-10 20:18:57 +0530

    Add files via upload

--- frontend/src/API.jsx
+++ frontend/src/API.jsx
@@ -2,5 +2,16 @@ import axios from 'axios'
 import React from 'react'
 
 export const API = axios.create({
-  baseURL : "https://todolist-usingexpressandmongo.onrender.com/"
+  baseURL : "https://todoapp-react-backend-yumj.onrender.com",
+  // baseURL : "http://localhost:5000"
 });
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
